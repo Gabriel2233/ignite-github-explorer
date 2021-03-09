4f8b44a7f30cbc70bf942f39c1f6221d02b54403
Gabriel Tiso <gabrieltisovinhasdebrito2@gmail.com>
2021-03-08 22:15:43 -0300

    feat: chapters one and two done

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,10 @@
+export default function RepositoryItem() {
+  return (
+    <li>
+      <strong>react-hook-form</strong>
+      <p>its cool</p>
+
+      <a href="">Link aqui</a>
+    </li>
+  );
+}
